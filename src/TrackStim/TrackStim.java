b3b5bcf9ee513c6776e051d59be87ea405557406
Dylan Fong <dylanfong.ut@gmail.com>
2019-10-15 12:43:15 -0400

    sequence acquisition working

--- src/TrackStim/TrackStim.java
+++ src/TrackStim/TrackStim.java
@@ -89,51 +89,56 @@ public class TrackStim implements SciJavaPlugin, MenuPlugin {
 
    // alternative way to process images
    public void startImageProcessingLoopAlt() {
-      int numImages = 100;
-      int curImage = 0;
-      double intervalMs = 100.0;
+      int nrFrames = 10;
+      double exposureMs = 100.00;
+      String savePath = "C:\\Users\\Mei Zhen\\Desktop\\temp4\\a\\";
+
+      // Create a Datastore for the images to be stored in, in RAM.
       Datastore store = dm.createRAMDatastore();
+      // Create a display to show images as they are acquired.
       DisplayWindow ds = dism.createDisplay(store);
 
+      Coords.CoordsBuilder builder = dm.getCoordsBuilder().time(0);
+
+      // Start collecting images.
+      // Arguments are the number of images to collect, the amount of time to wait
+      // between images, and whether or not to halt the acquisition if the
+      // sequence buffer overflows.
+
       try {
-         mmc.startSequenceAcquisition(numImages, intervalMs, false);
-      } catch (java.lang.Exception e) {
-         log.logMessage("error starting sequence acquisition");
-         log.logMessage(e.getMessage());
-      }
-
-      Coords.Builder builder = Coordinates.builder().z(0).channel(0).stagePosition(0);
-
-      while(mmc.isSequenceRunning()) {
-         int imagesRemaining = mmc.getRemainingImageCount();
-         log.logMessage(String.valueOf(imagesRemaining) + " images in queue");
-
-         if( imagesRemaining > 0 ){
-
-            try {
-               TaggedImage tImg = mmc.popNextTaggedImage();
-               Image img = studio.data().convertTaggedImage(
-                  tImg,
-                  builder.time(curImage).build(),
-                  null
-               );
-               store.putImage(img);
-               curImage++;
-            } catch (java.lang.Exception e ){
-               log.logMessage(e.getMessage());
+         mmc.startSequenceAcquisition(nrFrames, 0, true);
+         // Set up a Coords.CoordsBuilder for applying coordinates to each image.
+         int curFrame = 0;
+         while (mmc.getRemainingImageCount() > 0 || mmc.isSequenceRunning(mmc.getCameraDevice())) {
+            if (mmc.getRemainingImageCount() > 0) {
+               TaggedImage tagged = mmc.popNextTaggedImage();
+               // Convert to an Image at the desired timepoint.
+               Image image = dm.convertTaggedImage(tagged,
+                  builder.time(curFrame).build(), null);
+               store.putImage(image);
+               curFrame++;
+            }
+            else {
+               // Wait for another image to arrive.
+               mmc.sleep(Math.min(.5 * exposureMs, 20));
             }
          }
-         log.logMessage("sequence running");
-      }
 
-      try {
          mmc.stopSequenceAcquisition();
-      } catch (java.lang.Exception e ){
+      } catch ( java.lang.Exception e ){
+         log.logMessage("error");
+         log.logMessage(e.getMessage());
+      }
+      // Have Micro-Manager handle logic for ensuring data is saved to disk.
+      try {
+         store.save(Datastore.SaveMode.SINGLEPLANE_TIFF_SERIES, savePath);
+      } catch (java.io.IOException e){ 
+         log.logMessage("error saving");
          log.logMessage(e.getMessage());
       }
       dism.manage(store);
-   }
 
+   }
 
    /**
     * This method determines which sub-menu of the Plugins menu we are placed
