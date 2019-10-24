cc364addcf26a5c51588c450c4238859e820c62b
zhenlab mini <zhenlabmini@zhenlabs-Mac-mini.local>
2019-10-24 09:04:55 -0400

    change java imports to be explicit - instead fo ij.*, ij.Ij, ij.ImagePlus etc.

--- src/TrackStimPlugin.java
+++ src/TrackStimPlugin.java
@@ -1,22 +1,62 @@
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.prefs.Preferences;
-import java.util.concurrent.*;
-import java.io.*;
-import java.awt.*;
-import java.awt.event.*;
 
-import ij.*;
-import ij.io.*;
-import ij.process.*;
-import ij.gui.*;
-import ij.plugin.*;
-import ij.plugin.filter.*;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+import java.io.File;
+import java.io.OutputStream;
+import java.io.FileOutputStream;
+
+import java.awt.TextField;
+import java.awt.Label;
+import java.awt.GridBagConstraints;
+import java.awt.Dimension;
+import java.awt.Checkbox;
+import java.awt.Rectangle;
+import java.awt.Point;
+import java.awt.Button;
+import java.awt.Choice;
+import java.awt.GridBagLayout;
+
+import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ItemEvent;
+
+import ij.ImageListener;
+import ij.ImagePlus;
+import ij.IJ;
+import ij.WindowManager;
+import ij.ImageStack;
+
+import ij.io.DirectoryChooser;
+import ij.io.FileInfo;
+import ij.io.TiffEncoder;
+
+import ij.process.ImageProcessor;
+import ij.process.ImageStatistics;
+
+import ij.gui.Roi;
+import ij.gui.Wand;
+import ij.gui.ImageCanvas;
+import ij.gui.ImageWindow;
+import ij.gui.PolygonRoi;
+
+import ij.plugin.filter.EDM;
+import ij.plugin.filter.RankFilters;
 import ij.plugin.frame.PlugInFrame;
 
 import mmcorej.CMMCore;
 import mmcorej.MMCoreJ;
 import mmcorej.StrVector;
-import org.micromanager.*;
 import mmcorej.Configuration;
 import mmcorej.PropertySetting;
 
