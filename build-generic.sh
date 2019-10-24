404dc66da35cf1b10c5f78949bdebfea88d2b407
Dylan Fong <dylanfong.ut@gmail.com>
2019-10-24 11:38:10 -0400

    update build scripts to remove old dist artifacts before building again

--- build-generic.sh
+++ build-generic.sh
@@ -1,3 +1,4 @@
+rm ./dist/*
 cd ./src/
 javac -source 1.6 -target 1.6 -classpath /Applications/Micro-Manager1.4/ij.jar:/Applications/Micro-Manager1.4/plugins/Micro-Manager/MMCoreJ.jar:/Applications/Micro-Manager1.4/plugins/Micro-Manager/MMJ_.jar -Xlint:unchecked -sourcepath . -d ../dist TrackStimPlugin.java
 
