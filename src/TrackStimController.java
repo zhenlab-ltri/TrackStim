530e7da32c062c092566a68a7d0882c2c91a281a
Dylan Fong <dylanfong.ut@gmail.com>
2019-12-02 15:09:02 -0500

    add public/private to methods to improve readability

--- src/TrackStimController.java
+++ src/TrackStimController.java
@@ -13,14 +13,13 @@ import org.micromanager.api.ScriptInterface;
 
 
 class TrackStimController {
-    ArrayList<ScheduledFuture> snapshotTasks;
-    ArrayList<ScheduledFuture> stimulatorTasks;
+    private ArrayList<ScheduledFuture> snapshotTasks;
+    private ArrayList<ScheduledFuture> stimulatorTasks;
+    private String stimulatorPort;
 
     CMMCore core;
     ScriptInterface app;
     
-    String stimulatorPort;
-
 
     TrackStimController(CMMCore core_, ScriptInterface app_){
         core = core_;
@@ -29,7 +28,52 @@ class TrackStimController {
         stimulatorTasks = new ArrayList<ScheduledFuture>();
     }
 
-    String createImageSaveDirectory(String root){
+    public void startImageAcquisition(int numFrames, int framesPerSecond, String rootDirectory){
+
+        // create a new directory in rootDirectory to save images to
+        String imageSaveDirectory = createImageSaveDirectory(rootDirectory);
+
+        // ensure micro manager live mode is on so we can capture images
+        if( !app.isLiveModeOn() ){
+            app.enableLiveMode(true);
+        }
+
+        snapshotTasks = scheduleSnapShots(numFrames, framesPerSecond, imageSaveDirectory);
+    }
+
+    public void stopImageAcquisition(){
+        // cancel getting images
+        for (int i = 0; i < snapshotTasks.size(); i++ ){
+            snapshotTasks.get(i).cancel(true);
+        }
+
+        // cancel turning on the stimulator
+        for (int j = 0; j < stimulatorTasks.size(); j++ ){
+            snapshotTasks.get(j).cancel(true);
+        }
+    }
+
+    // schedule a number of snapshot at fixed time interval to ensure that images are taken
+    // at the given fps
+    private ArrayList<ScheduledFuture> scheduleSnapShots(int numFrames, int fps, String saveDirectory){
+        ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
+        ArrayList<ScheduledFuture> futureTasks = new ArrayList<ScheduledFuture>();
+        fps = 10; // fix fps at 10 for now
+
+        long frameCycleNano = TimeUnit.MILLISECONDS.toNanos(1000 / fps); // take a pic every 100ms
+
+        for(int curFrameIndex = 0; curFrameIndex < numFrames; curFrameIndex++){
+            long timePtNano = curFrameIndex * frameCycleNano; // e.g. 0 ms, 100ms, 200ms, etc..
+            ScheduledSnapShot s = new ScheduledSnapShot(core, app, timePtNano, saveDirectory, curFrameIndex);
+
+            ScheduledFuture snapShot = ses.schedule(s, timePtNano, TimeUnit.NANOSECONDS);
+            futureTasks.add(snapShot);
+        }
+
+        return futureTasks;
+    }
+    
+    private String createImageSaveDirectory(String root){
         // get count number of directories N so that we can create directory N+1
         File saveDirectoryFile = new File(root);
         File[] fileList = saveDirectoryFile.listFiles();
@@ -52,47 +96,4 @@ class TrackStimController {
         return newdir.getPath();
     }
 
-    void startImageAcquisition(int numFrames, int framesPerSecond, String rootDirectory){
-
-        // create a new directory in rootDirectory to save images to
-        String imageSaveDirectory = createImageSaveDirectory(rootDirectory);
-
-        // ensure micro manager live mode is on so we can capture images
-        if( !app.isLiveModeOn() ){
-            app.enableLiveMode(true);
-        }
-
-        snapshotTasks = scheduleSnapShots(numFrames, framesPerSecond, imageSaveDirectory);
-    }
-
-    void stopImageAcquisition(){
-        // cancel getting images
-        for (int i = 0; i < snapshotTasks.size(); i++ ){
-            snapshotTasks.get(i).cancel(true);
-        }
-
-        // cancel turning on the stimulator
-        for (int j = 0; j < stimulatorTasks.size(); j++ ){
-            snapshotTasks.get(j).cancel(true);
-        }
-    }
-
-    // schedule a number of snapshot at fixed time interval (computed via fps)
-    ArrayList<ScheduledFuture> scheduleSnapShots(int numFrames, int fps, String saveDirectory){
-        ScheduledExecutorService ses = Executors.newSingleThreadScheduledExecutor();
-        ArrayList<ScheduledFuture> futureTasks = new ArrayList<ScheduledFuture>();
-        fps = 10; // fix fps at 10 for now
-
-        long frameCycleNano = TimeUnit.MILLISECONDS.toNanos(1000 / fps); // take a pic every 100ms
-
-        for(int curFrameIndex = 0; curFrameIndex < numFrames; curFrameIndex++){
-            long timePtNano = curFrameIndex * frameCycleNano; // e.g. 0 ms, 100ms, 200ms, etc..
-            ScheduledSnapShot s = new ScheduledSnapShot(core, app, timePtNano, saveDirectory, curFrameIndex);
-
-            ScheduledFuture snapShot = ses.schedule(s, timePtNano, TimeUnit.NANOSECONDS);
-            futureTasks.add(snapShot);
-        }
-
-        return futureTasks;
-    }    
 }
